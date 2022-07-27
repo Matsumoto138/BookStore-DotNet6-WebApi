686de84149c6b2a390e98eb084886a450d48511d
Fatih Demirbas <dmrbs.fd@gmail.com>
2022-07-27 12:29:41 +0300

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.DBOperations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookStoreDbContext _contex;
+
+        public BookController(BookStoreDbContext contex)
+        {
+            _contex = contex;
+        }
+
+        // Get
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = _contex.Books.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+
+        [HttpGet("{id}")]
+
+        public Book GetById(int id)
+        {
+            var book = _contex.Books.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+        }
+
+        // Post
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = _contex.Books.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book != null)
+                return BadRequest();
+
+
+            _contex.Books.Add(newBook);
+            _contex.SaveChanges();
+            return Ok();
+        }
+
+        // Put
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = _contex.Books.SingleOrDefault(x => x.Id == id);
+
+            if (book == null)
+                return BadRequest();
+
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : default;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : default;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : default;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : default;
+
+            _contex.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteBook(int id)
+        {
+            var book = _contex.Books.SingleOrDefault(x => x.Id == id);
+            if (book == null)
+                return BadRequest();
+
+            _contex.Books.Remove(book);
+            _contex.SaveChanges();
+            return Ok();
+
+        }
+
+    }
+}
---
+++ Migrations/20220726124433_InitialDatabaseMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class InitialDatabaseMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    PageCount = table.Column<int>(type: "int", nullable: false),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
